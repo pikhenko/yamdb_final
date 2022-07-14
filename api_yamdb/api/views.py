d319948a67c8e0926f441c8e1ac06e87e091d035
Артем Пихенько <apikhenko@yandex.ru>
2022-07-14 03:19:46 +0300

    fix2

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -8,7 +8,6 @@ from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from reviews.models import Category, Genre, Review, Title
 from user.models import User
 
